e0aa34636bc6271d5fdf3af925a51f502de338f3
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2024-03-29 16:19:39 -0500

    Create base styles for dialog in the overall aplication

---
+++ src/app/util/components/overall_dialog_structure/overall-dialog-structure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-dialog-structure',
+  templateUrl: './overall-dialog-structure.component.html',
+  styleUrls: ['./overall-dialog-structure.component.sass'],
+})
+export class OverallDialogStructureComponent {
+  protected styleType = 'warning';
+}

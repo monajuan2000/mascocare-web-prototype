98ae7b6bf8d9c1739b7d74d70011ea49856cef5d
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2024-05-26 18:54:42 -0500

    Finish basic structure for  owners sign up components

--- src/app/util/owners/owners.module.ts
+++ src/app/util/owners/owners.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OwnersSignUpComponent } from './owners_sign_up/owners-sign-up.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [OwnersSignUpComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InputTextModule],
 })
 export class OwnersModule {}

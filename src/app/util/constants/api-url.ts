70bfb2426772e3282f835fd66b71ccec3b52b7b7
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2024-06-11 15:30:01 -0500

    Change some things for improve readability, and maintainability

--- src/app/util/constants/api-url.ts
+++ src/app/util/constants/api-url.ts
@@ -1,7 +1,13 @@
 interface ApiUrls {
   LOGIN_URL: string;
+  ALL_OWNERS_URL: string;
 }
 
+const baseUrl = {
+  BASE_URL: 'http://localhost:8080/mascocarev1-api/',
+};
+
 export const apiUrl: ApiUrls = {
-  LOGIN_URL: 'http://localhost:8080/mascocarev1-api/login',
+  LOGIN_URL: baseUrl.BASE_URL + 'login',
+  ALL_OWNERS_URL: baseUrl.BASE_URL + 'util/owners/getAllOwners?version=1',
 };
--- src/app/util/owners/services/owner.service.ts
+++ src/app/util/owners/services/owner.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { apiUrl } from '../../constants/api-url';
 
 @Injectable({ providedIn: 'root' })
 export class OwnerService {
@@ -10,11 +11,8 @@ export class OwnerService {
     const username = 'monajuan2000_user';
     const password = 'MonaBermudez30';
     const authHeader = 'Basic ' + btoa(username + ':' + password);
-    return this.http.get(
-      'http://localhost:8080/mascocarev1-api/util/owners/getAllOwners?version=1',
-      {
-        headers: { Authorization: authHeader },
-      }
-    );
+    return this.http.get(apiUrl.ALL_OWNERS_URL, {
+      headers: { Authorization: authHeader },
+    });
   }
 }

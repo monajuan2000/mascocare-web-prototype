e49d64c907d8deb6f3d2f97c306683a71e49a36c
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2024-06-12 14:10:37 -0500

    Create basic structure, and grid for signup component

--- src/app/util/owners/owners_sign_up/owners-sign-up.component.ts
+++ src/app/util/owners/owners_sign_up/owners-sign-up.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { CalendarModule } from 'primeng/calendar';
-import { FormsModule } from '@angular/forms';
-import { RadioButtonModule } from 'primeng/radiobutton';
+import { OwnersSignupInputComponent } from '../components/owners-signup-input/owners-signup-input.component';
+import { MainButtonComponent } from '../../components/main_button/main-button.component';
 
 @Component({
   selector: 'app-owners-sign-up',
   templateUrl: './owners-sign-up.component.html',
   styleUrls: ['./owners-sign-up.component.sass'],
   standalone: true,
-  imports: [CalendarModule, FormsModule, RadioButtonModule],
+  imports: [OwnersSignupInputComponent, MainButtonComponent],
 })
-export class OwnersSignUpComponent {
-  protected ownersDate: Date | undefined;
-}
+export class OwnersSignUpComponent {}

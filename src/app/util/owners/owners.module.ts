6a0766bb7f7e88ff7ba5e6e88c90655c9409df5b
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2024-06-14 10:00:04 -0500

    Create basic structure, and grid for signup component

--- src/app/util/owners/owners.module.ts
+++ src/app/util/owners/owners.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { RadioButtonModule } from 'primeng/radiobutton';
+import { OwnersContactInputComponent } from './components/owners-contact-input/owners-contact-input.component';
+import { OwnersPetInputComponent } from './components/owners-pet-input/owners-pet-input.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [OwnersContactInputComponent, OwnersPetInputComponent],
   imports: [CommonModule, InputTextModule, RadioButtonModule],
 })
 export class OwnersModule {}
--- src/app/util/owners/owners_sign_up/owners-sign-up.component.ts
+++ src/app/util/owners/owners_sign_up/owners-sign-up.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { OwnersSignupInputComponent } from '../components/owners-signup-input/owners-signup-input.component';
 import { MainButtonComponent } from '../../components/main_button/main-button.component';
+import { OwnersBasicInputComponent } from '../components/owners-basic-input/owners-basic-input.component';
+import { OwnersContactInputComponent } from '../components/owners-contact-input/owners-contact-input.component';
 
 @Component({
   selector: 'app-owners-sign-up',
   templateUrl: './owners-sign-up.component.html',
   styleUrls: ['./owners-sign-up.component.sass'],
   standalone: true,
-  imports: [OwnersSignupInputComponent, MainButtonComponent],
+  imports: [
+    OwnersBasicInputComponent,
+    OwnersContactInputComponent,
+    MainButtonComponent,
+  ],
 })
 export class OwnersSignUpComponent {}
